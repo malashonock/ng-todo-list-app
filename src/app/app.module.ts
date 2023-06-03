c6a19165d321b547c4d5c3bbe59f4cefa47c0bb5
Yan Malashonak <yan.malashonak@gmail.com>
2023-06-04 00:27:55 +0300

    refactor: implement separate todo-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddTodoComponent,
     TodoItemComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent {
+  @Input() todoItems!: string[];
+  @Output() taskDone = new EventEmitter<number>();
+}
