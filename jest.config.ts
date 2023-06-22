2c03837273a6d8b698a1d5da93f58f41bf37d1a6
Yan Malashonak <yan.malashonak@gmail.com>
2023-06-22 16:33:02 +0300

    init: setup jest & remove  jasmine+karma

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'jest';
+
+const jestConfig: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+};
+
+export default jestConfig;
