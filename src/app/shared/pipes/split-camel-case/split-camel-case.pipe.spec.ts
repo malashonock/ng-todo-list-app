ac054359b3f6f617ee47cd27b07dd80fabb7e469
Yan Malashonak <yan.malashonak@gmail.com>
2023-06-22 17:52:05 +0300

    test(shared/pipes): write tests for SplitCamelCase pipe

--- src/app/shared/pipes/split-camel-case/split-camel-case.pipe.spec.ts
+++ src/app/shared/pipes/split-camel-case/split-camel-case.pipe.spec.ts
@@ -1,8 +1,25 @@
 import { SplitCamelCasePipe } from './split-camel-case.pipe';
 
 describe('SplitCamelCasePipe', () => {
+  let pipe: SplitCamelCasePipe;
+
+  beforeEach(() => {
+    pipe = new SplitCamelCasePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new SplitCamelCasePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('given an empty string, should return empty string', () => {
+    expect(pipe.transform('')).toBe('');
+  });
+
+  it('should split a camelCasePhrase into separate words', () => {
+    expect(pipe.transform('camelCasePhrase')).toBe('camel Case Phrase');
+  });
+
+  it('should ignore spaces and special characters', () => {
+    expect(pipe.transform('Hello, world!')).toBe('Hello, world!');
   });
 });
