303d1723002186d3efe6bf3aa5101a2232e76179
Yan Malashonak <yan.malashonak@gmail.com>
2023-06-22 17:44:35 +0300

    test(shared/pipes): write tests for SentenceCase pipe

--- src/app/shared/pipes/sentence-case/sentence-case.pipe.spec.ts
+++ src/app/shared/pipes/sentence-case/sentence-case.pipe.spec.ts
@@ -1,8 +1,25 @@
 import { SentenceCasePipe } from './sentence-case.pipe';
 
 describe('SentenceCasePipe', () => {
+  let pipe: SentenceCasePipe;
+
+  beforeEach(() => {
+    pipe = new SentenceCasePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new SentenceCasePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('given an empty string, should return empty string', () => {
+    expect(pipe.transform('')).toBe('');
+  });
+
+  it('given the 1st letter is lowercase, should capitalize only the 1st letter', () => {
+    expect(pipe.transform('hello, world!')).toBe('Hello, world!');
+  });
+
+  it('given the 1st letter is uppercase, should not change anything', () => {
+    expect(pipe.transform('Hello, world!')).toBe('Hello, world!');
   });
 });
