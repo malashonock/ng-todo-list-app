976404b967007338a6def4913b272725ab984fd1
Yan Malashonak <yan.malashonak@gmail.com>
2023-06-22 17:28:02 +0300

    test(core/interceptors): write tests for API interceptor

--- src/app/core/interceptors/api.interceptor.spec.ts
+++ src/app/core/interceptors/api.interceptor.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
 import { ApiInterceptor } from './api.interceptor';
+import { environment } from 'environments/environment';
 
 describe('ApiInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      ApiInterceptor
-      ]
-  }));
+  let apiInterceptor: ApiInterceptor;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true }
+      ],
+    });
+    apiInterceptor = TestBed.inject(HTTP_INTERCEPTORS)[0];
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const interceptor: ApiInterceptor = TestBed.inject(ApiInterceptor);
-    expect(interceptor).toBeTruthy();
+    expect(apiInterceptor).toBeTruthy();
+  });
+
+  it('should intercept HTTP requests', () => {
+    const relativeUrl = '/todos';
+    const fullUrl = environment.API_BASE_URL + relativeUrl;
+
+    httpClient.get(relativeUrl).subscribe();
+
+    const req = httpTestingController.expectOne(fullUrl);
+    httpTestingController.verify();
   });
 });
