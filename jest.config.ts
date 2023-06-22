7642899c46f1e0debb269944bcf5f03c44cbb998
Yan Malashonak <yan.malashonak@gmail.com>
2023-06-22 16:55:57 +0300

    fix(init): add baseUrl to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,8 @@ const jestConfig: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  transformIgnorePatterns: ['node_modules/(?!@angular|rjxs|@ngrx)'],
   collectCoverage: true,
   coverageDirectory: '<rootDir>/coverage',
 };
