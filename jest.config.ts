aa6201cd7e2dade778269b1fbd32df39c719767c
Yan Malashonak <yan.malashonak@gmail.com>
2023-06-23 11:40:21 +0300

    init(jest): remove jest preset global setup from config

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import type { Config } from 'jest';
 const jestConfig: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  globalSetup: 'jest-preset-angular/global-setup',
   moduleDirectories: ['node_modules', '<rootDir>/src'],
   transformIgnorePatterns: ['node_modules/(?!@angular|rjxs|@ngrx)'],
   collectCoverage: true,
